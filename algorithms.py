fe35702b0571ef9a487588f18d3667f5c87242f9
Rafilde <rafasilva182003@gmail.com>
2025-12-06 17:43:26 -0300

    feat: implementando o algoritmo flooding

--- algorithms.py
+++ algorithms.py
@@ -18,7 +18,38 @@ class SearchEngine:
             return None
 
     def _flooding(self, start_node, target_resource, ttl):
-        pass
+        msgs_count = 0
+        nodes_involved = set()
+        
+        queue = [(start_node, ttl)]
+        visited = {start_node}
+        nodes_involved.add(start_node)
+
+        while queue:
+            current_node, current_ttl = queue.pop(0)
+            
+            if target_resource in self.graph.nodes[current_node]['resources']:
+                print(f"!!! ENCONTRADO no nó {current_node} !!!")
+                return {
+                    "success": True,
+                    "msgs": msgs_count,
+                    "nodes": len(nodes_involved),
+                    "final_node": current_node
+                }
+
+            if current_ttl <= 0:
+                continue
+
+            neighbors = list(self.graph.neighbors(current_node))
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    msgs_count += 1
+                    visited.add(neighbor)
+                    nodes_involved.add(neighbor)
+                    queue.append((neighbor, current_ttl - 1))
+
+        print("XXX Não encontrado XXX")
+        return {"success": False, "msgs": msgs_count, "nodes": len(nodes_involved)}
 
     def _informed_flooding(self, start_node, target_resource, ttl):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from algorithms import SearchEngine
 from p2p_network import P2PNetwork
 
 def main():
@@ -8,6 +9,21 @@ def main():
         return
     
     # rede.draw() 
+
+    search = SearchEngine(rede.graph)
+
+    result = search.run_search(
+        start_node='n1',
+        target_resource="jogo_se.exe",
+        ttl=10,
+        algo='flooding'
+    )
+
+    print("\nRELATÓRIO FINAL:")
+    print(f"Sucesso: {result['success']}")
+    print(f"Mensagens Trocadas: {result['msgs']}")
+    print(f"Nós envolvidos: {result['nodes']}")
+    print(f"Nó final: {result.get('final_node', 'Nenhum')}")
 
 if __name__ == "__main__":
     main()
