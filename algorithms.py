36e2f347c56608b3000875adba0424fcdcdb32ed
Rafilde <rafasilva182003@gmail.com>
2025-12-06 20:01:54 -0300

    feat: implementei informed random walk

--- algorithms.py
+++ algorithms.py
@@ -89,8 +89,55 @@ class SearchEngine:
         print("XXX Não encontrado (TTL acabou ou beco sem saída) XXX")
         return {"success": False, "msgs": msgs_count, "nodes": len(nodes_involved)}
 
+    # Bom validar esse antes
     def _informed_random_walk(self, start_node, target_resource, ttl):
-        pass
+        msgs_count = 0
+        nodes_involved = set()
+        
+        queue = [(start_node, ttl, None)]
+        
+        nodes_involved.add(start_node)
+
+        while queue:
+            current_node, current_ttl, previous_node = queue.pop(0)
+
+            print(f"-> Visitando: {current_node} (TTL: {current_ttl}, Veio de: {previous_node})")
+            
+            if target_resource in self.graph.nodes[current_node]['resources']:
+                print(f"!!! ENCONTRADO no nó {current_node} !!!")
+                return {
+                    "success": True, 
+                    "msgs": msgs_count, 
+                    "nodes": len(nodes_involved), 
+                    "final_node": current_node
+                }
+
+            if current_ttl <= 0:
+                continue
+
+            neighbors = list(self.graph.neighbors(current_node))
+            
+            if neighbors:
+  
+                novos_vizinhos = [n for n in neighbors if n != previous_node]
+                
+                best_neighbor = None
+
+                if novos_vizinhos:
+                    random.shuffle(novos_vizinhos) 
+                    best_neighbor = max(novos_vizinhos, key=lambda n: len(list(self.graph.neighbors(n))))
+                
+                else:
+                    best_neighbor = previous_node
+                
+                if best_neighbor:
+                    msgs_count += 1
+                    nodes_involved.add(best_neighbor)
+                    
+                    queue.append((best_neighbor, current_ttl - 1, current_node))
+                
+        print("XXX Não encontrado (TTL acabou) XXX")
+        return {"success": False, "msgs": msgs_count, "nodes": len(nodes_involved)}
 
     def _informed_flooding(self, start_node, target_resource, ttl):
         pass
